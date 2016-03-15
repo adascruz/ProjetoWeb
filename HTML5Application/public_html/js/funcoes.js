09eef80099d349205073c6dd44588e99e51f2de1
raquelcina <Ada@10.10.15.166>
2016-03-15 18:09:38 -0300

    Redimensionando Iframe

---
+++ HTML5Application/public_html/js/funcoes.js
@@ -0,0 +1,16 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+function redimensionaIframe(id){
+    if (obj = parent.document.getElementById(id)) {
+        obj.height = document.body.scrollHeight + 10;
+    }
+}
+
+
+function validaFaleConosco(){
+    
+}
