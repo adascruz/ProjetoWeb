121d938a8311aabc493eea380811d3104a1a40b5
raquelcina <Ada@Ada-PC>
2016-03-16 15:38:30 -0300

    Finalizando Inicial, Contato. Paginas linkadas.

--- HTML5Application/public_html/js/funcoes.js
+++ HTML5Application/public_html/js/funcoes.js
@@ -3,14 +3,44 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+/*
+ Código JQuery
+ */
+jQuery(function ($) {
+    $("#txtCpf").mask("999.999.999.99");
+    $("#txtFone").mask("(99)9999-9999");
 
-function redimensionaIframe(id){
+});
+
+function redimensionaIframe(id) {
     if (obj = parent.document.getElementById(id)) {
         obj.height = document.body.scrollHeight + 10;
     }
 }
 
 
-function validaFaleConosco(){
+/*função inútil*/
+function validarMensagem() {
     
+    var nome = document.getElementById('txtNome').value;
+    var msgm = document.getElementById('txtMensagem').value;
+
+    if (nome === " ") {
+        alert("Informe seu nome!");
+        //return alert()
+    } else if (msgm==" "){
+        alert("Escreva uma mensagem!");
+    }
+    else{
+        return true;
+    }
+    /*if (email == "") {
+        alert("Informe seu email!");
+        return false;
+    } 
+    if (cpf == "") {
+        alert("Informe a CPF!");
+        return false;
+    } */
+        
 }
